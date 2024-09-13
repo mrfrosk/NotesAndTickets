a334e21bc33d59cb4c80db1760b7d4c44a3d50a1
mrfrosk <heroker44@gmail.com>
2024-09-13 22:04:58 +0300

    Создал JwtProperties, хранящий свойства jwt из yaml

--- src/main/kotlin/com/example/first/FirstApplication.kt
+++ src/main/kotlin/com/example/first/FirstApplication.kt
@@ -1,11 +1,15 @@
 package com.example.first
 
+import com.example.first.configuration.yaml.JwtProperties
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties::class)
 @ImportAutoConfiguration(
 	exclude = [DataSourceTransactionManagerAutoConfiguration::class]
 )
