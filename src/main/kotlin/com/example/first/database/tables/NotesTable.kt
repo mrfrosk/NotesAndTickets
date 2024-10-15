9b93e72ab328764454b434fbd022325ae8728621
mrfrosk <heroker44@gmail.com>
2024-10-15 16:02:50 +0300

    Добавил коскадное удаление в NotesTables, очистил NoteServiceTest

--- src/main/kotlin/com/example/first/database/tables/NotesTable.kt
+++ src/main/kotlin/com/example/first/database/tables/NotesTable.kt
@@ -2,9 +2,10 @@ package com.example.first.database.tables
 
 import com.example.first.configuration.Config
 import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.ReferenceOption
 
 object NotesTable: UUIDTable("${Config.SCHEMA_NAME}.notes") {
     val title = varchar("title",200).uniqueIndex()
     val text = text("text")
-    val user = reference("user", UsersTable.id)
+    val user = reference("user", UsersTable.id, ReferenceOption.CASCADE, ReferenceOption.CASCADE)
 }
